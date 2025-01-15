028b9ac819d58ece9275d56adea46c669213e31d
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-15 14:32:48 +0100

    Added show and started new + create  routes

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,4 +2,22 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @id = params[:id]
+    @restaurant = Restaurant.find(@id)
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  end
+
+  private
+
+  def form_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/restaurants", to: "restaurants#index"
+  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
+  get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
+  post "/restaurants", to: "restaurants#create"
 end
