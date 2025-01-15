99d153071bc7f7b562ecedfd97d8019b04919c2a
Simo Paraschi <58208573+simoparaschi@users.noreply.github.com>
2025-01-15 16:00:02 +0100

    Displaying reviews now in show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,12 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @id = params[:id]
-    @restaurant = Restaurant.find(@id)
+    @reviews = @restaurant.reviews
   end
 
   def new
@@ -13,9 +14,17 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new(form_params)
+    @restaurant.save
+    redirect_to restaurants_path()
   end
 
   private
+
+  def find_restaurant
+    @id = params[:id]
+    @restaurant = Restaurant.find(@id)
+  end
 
   def form_params
     params.require(:restaurant).permit(:name, :address, :category)
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/restaurants", to: "restaurants#index"
-  get "/restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "/restaurants/new", to: "restaurants#new", as: :new_restaurant
-  post "/restaurants", to: "restaurants#create"
+  resources :restaurants
+
 end
